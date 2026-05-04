055e17a902add1c086ba410ab51e293a8523d40a
Adhisetama <adhisetama@gmail.com>
2026-05-04 20:03:03 +0700

    chore: set up tailwind custom colors and fonts

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    blue1: '#006CE4',
+                    blue2: '#004898',
+
+                    paleblue: '#F3F8FF',
+                    darkblue: '#00317C',
+                },
+                neutral: {
+                    dark: '#212529',
+                }
+            },
+        },
+    },
+}
