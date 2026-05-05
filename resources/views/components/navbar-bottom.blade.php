e0b807c756c1465e239ef759a47ba95a3ccf8234
Adhisetama <adhisetama@gmail.com>
2026-05-06 06:08:39 +0700

    feat: bottom nav component for mobile

---
+++ resources/views/components/navbar-bottom.blade.php
@@ -0,0 +1,27 @@
+<nav class="fixed bottom-0 left-0 right-0 z-50 bg-white md:hidden">
+    <div class="grid grid-cols-4 text-xs py-1">
+
+        <a href="#" class="flex flex-col items-center justify-center text-primary-blue1">
+            <span class="ei-home text-3xl"></span>
+            <span>Home</span>
+        </a>
+
+        <a href="#" class="flex flex-col items-center justify-center">
+            <span class="ei-my_class text-3xl text-neutral-lightgray"></span>
+            <span>My Class</span>
+        </a>
+
+        <a href="#" class="flex flex-col items-center justify-center">
+            <lottie-player src="{{ asset('images/lottie/diskon.json') }}" background="transparent" speed="1"
+                style="width:30px;height:30px;" loop autoplay>
+            </lottie-player>
+            <span>Promo</span>
+        </a>
+
+        <a href="#" class="flex flex-col items-center justify-center">
+            <span class="ei-payment text-3xl text-neutral-lightgray"></span>
+            <span>Transaction</span>
+        </a>
+
+    </div>
+</nav>
