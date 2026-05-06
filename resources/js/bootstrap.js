c217c228a2bc28f192d5e0e91f33a44c541c879c
Adhisetama <adhisetama@gmail.com>
2026-05-06 11:01:58 +0700

    feat: add program recommendation section in home page

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import Alpine from 'alpinejs';
 import '@lottiefiles/lottie-player';
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 // axios
 window.axios = axios;
@@ -9,3 +13,31 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // alpine
 window.Alpine = Alpine;
 Alpine.start();
+
+// swiper
+const getSlidesPerView = (query, slideWidth, maxSlides) => {
+    const clientWidth = document.querySelector(query).clientWidth;
+    return Math.min(maxSlides, clientWidth / slideWidth);
+}
+{
+    // program recommendation swiper
+    const query = '#pr-swiper'
+    new Swiper(query, {
+        modules: [Pagination],
+        slidesPerView: getSlidesPerView(query, 250, 4),
+        spaceBetween: 10,
+        breakpoints: {
+            1024: {
+                slidesPerView: 4
+            }
+        },
+        pagination: {
+            el: `${query} .swiper-pagination`,
+            clickable: true,
+        },
+    });
+    const observer = new ResizeObserver(() => {
+        swiper.update();
+    });
+    observer.observe(document.querySelector(query))
+}
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,7 @@
 
     <x-partials.home.hero />
     <x-partials.home.program-choice />
+    <x-partials.home.program-recommendation />
 
     Home Page
 </x-layouts.app>
